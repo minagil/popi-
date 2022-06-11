ea856768875ed2c99a301a6e822ef12b1a072951
mina <rlfalsk0301@gmail.com>
2022-06-11 22:30:43 +0900

    add a sript.js

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+/* UICompoenet */
+var UIComponent = UIComponent || {};
+UIComponent.Event = {
+	"popupOpen":new CustomEvent("popupOpen"),
+	"popupClose":new CustomEvent("popupClose"),
+}
+
+UIComponent.Popup = function(props){
+  var _self = this;
+  var _target = null;
+  var _container = null;
+  var _popupDimmend = null;
+
+  function init(){
+    setPopup();
+    setButtons();
+  }
+
+  function setPopup(){
+    _target = props['target'];
+    _container = _target;
+
+    _container.addEventListener('popupOpen',function(){
+      console.log("ddd");
+      this.classList.add("show");
+      createDimmed();
+    });
+    
+    _container.addEventListener('popupClose',function(){
+      this.classList.remove("show");
+    });
+  }
+
+  function setButtons(){
+    var closeBtn = _container.querySelector('.btn-popup-cancel');
+
+    if(closeBtn){
+      closeBtn.addEventListener('click',function(){
+        _self.close();
+      },false);
+    }
+  }
+
+  function createDimmed(){
+    _popupDimmend = document.createElement('div'); 
+    _popupDimmend.className = 'dimmed';
+    _target.appendChild(_popupDimmend);
+  }
+
+  this.open = function() {
+    _container.style.display = 'block';
+    _target.dispatchEvent(UIComponent.Event.popupOpen);
+  }
+
+  this.close = function(){
+    _container.style.display = 'none';
+    _target.dispatchEvent(UIComponent.Event.popupClose);
+  }
+
+  init();
+}
+
+
